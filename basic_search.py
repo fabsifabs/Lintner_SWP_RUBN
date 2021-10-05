0b417775de5d715686ce0a192779c19221d76ead
fabsifabs <fabian.lintner29@gmail.com>
2021-10-05 20:56:39 +0200

    Update basic_search.py

--- basic_search.py
+++ basic_search.py
@@ -1,24 +1,21 @@
 import time
 
 arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-print("Zahl:")
-n = int(input())
-print("zahl:",n)
+
 
 def interative_search():
     b = False
     d = 0
+    t = time.time_ns()
     for x in arr:
         if x == n:
 
             d +=1
-            print("on place ",x," is the number" )
+            print("Die Zahl:",n," steh im Array an Stelle: ",x, " Es brauchte ", d," Durchlaufe" )
             b = True
             print("Durchlaufe benoetigt:",d)
+            print("Zeit fuer die suche benoetigt:",time.time_ns()-t)
             break
-        else: 
-            #print("on place ",x, "is not the number")
-            b = False
 
     if b == False:
         print(n, "was not in the array")
@@ -36,17 +33,25 @@ def  binari_search():
         r = r+1
 
         if  n == arr[m]:
-            print ("Stelle im Array: ",m, " Durchlaeufe: ", r)
-            print("Zeit benoetigt:",time.time_ns()-t)
+            print ("Die Zahl:",n," steh im Array an Stelle: ",m, " Es brauchte ", r," Durchlaufe")
+            print("Zeit fuer die suche benoetigt:",time.time_ns()-t)
             return 
         elif n < arr[m]:
             e = m - 1
         elif n > arr[m]:
             s = m + 1 
 
-    
     print("Die gesuchte Zahl ist nicht enthalten!")
 
-
-binari_search()    
-#interative_search()
+print ("Interativ oder binaer? (tippe I/B)")
+i = input()
+if i == "B":
+    print("Gesuchte Zahl:")
+    n = int(input())
+    binari_search()  
+elif  i == "I"  :
+    print("Gesuchte Zahl:")
+    n = int(input())
+    interative_search()
+else: 
+    print("KEINE GÜLTIGE EINGABE!!!")
