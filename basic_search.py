a6d3fbe73333f0c2fa1b56fb2417cec6868229d8
fabsifabs <fabian.lintner29@gmail.com>
2021-10-05 20:42:14 +0200

    Update basic_search.py

--- basic_search.py
+++ basic_search.py
@@ -1,12 +1,16 @@
 import time
+
 arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
-#print (len(arr))
-n = 2133
+print("Zahl:")
+n = int(input())
+print("zahl:",n)
+
 def interative_search():
     b = False
     d = 0
     for x in arr:
         if x == n:
+
             d +=1
             print("on place ",x," is the number" )
             b = True
@@ -42,9 +46,6 @@ def  binari_search():
 
     
     print("Die gesuchte Zahl ist nicht enthalten!")
-
-
-
 
 
 binari_search()    
